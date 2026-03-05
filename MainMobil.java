41bcaf73a833c207ad335064c41566015b8f75ce
kalaa <164788792+dikaaaalaa@users.noreply.github.com>
2026-03-05 21:19:49 +0700

    Main Soal dari Class Mobil
    Instantiate two Mobil objects and display their details. Change the color of the first Mobil object and display the updated details.

---
+++ MainMobil.java
@@ -0,0 +1,30 @@
+public class MainMobil {
+    public static void main(String[] args) {
+        //instan objek bernama m1
+        Mobil m1= new Mobil();
+        m1.setKecepatan(50);
+        m1.setManufaktur("Toyota");
+        m1.setNoPlat("AB 1231 UA");
+        m1.setWarna("Merah");
+        m1.displayMessage();
+        System.out.println("===============================");
+
+        //instan objek bernama m2
+        Mobil m2= new Mobil();
+        m2.setKecepatan(100);
+        m2.setManufaktur("Mitsubishi");
+        m2.setNoPlat("N 1134 AG");
+        m2.setWarna("Pink");
+        m2.displayMessage();
+        System.out.println("===============================");  
+
+        //merubah warna dari objek m1
+
+        System.out.println("mobil pada m1 dirubah menjadi warna hijau");
+        m1.setWarna("Hijau");
+        //menampilkan hasil perubahan 
+        m1.displayMessage();
+
+    }
+
+}
