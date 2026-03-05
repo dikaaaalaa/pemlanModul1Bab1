20c151869f50ed61e9fa532ecae53d0bbf66b3cc
kalaa <164788792+dikaaaalaa@users.noreply.github.com>
2026-03-06 04:34:30 +0700

    rubahSekon Soal 5

---
+++ Mobil5.java
@@ -0,0 +1,35 @@
+public class Mobil5 {
+    private String noPlat;
+    private String warna;
+    private String manufaktur;
+    private int kecepatan;
+    private double waktu;
+
+    public void setNoPlat(String s) {
+        noPlat = s;
+    }
+    public void setWarna(String s) {
+        warna = s;
+    }
+    public void setManufaktur(String s) {
+        manufaktur = s;
+    }
+    public void setKecepatan(int i) {
+        kecepatan = i;
+    }
+    public void setWaktu(double w) {
+        waktu = rubahSekon(w);
+    }
+
+    private double rubahSekon(double jam) {
+        return jam * 3600;
+    }
+
+    public void displayMessage(){
+        System.out.println("Mobil anda adalah bermerk " + manufaktur);
+        System.out.println("Mempunyai nomor plat " + noPlat);
+        System.out.println("Memiliki warna " + warna);
+        System.out.println("dan mampu menempuh kecepatan " + kecepatan);
+        System.out.println("waktu perjalanan " + waktu + " detik");
+    }
+ }
